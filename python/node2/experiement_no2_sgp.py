bfee67edd220aaf0f414956cbb3634010032fe9a
Xiuming Liu <xiuming.liu@it.uu.se>
2019-06-22 11:31:52 +0200

    node2

--- python/node2/experiement_no2_sgp.py
+++ python/node2/experiement_no2_sgp.py
@@ -29,28 +29,27 @@ no2, nox, pm10, meteo, date = import_data.import_data()
 # =============================================================================
 # Pre-processing
 # =============================================================================
-pm10 = np.array(pm10).reshape(5, -1)
+no2 = np.array(no2).reshape(5, -1)
 meteo = np.array(meteo).reshape(5, -1)
 
 # Replace zeros with average
-pm10 = functions.replace_zero_with_average(pm10)
+no2 = functions.replace_zero_with_average(no2)
 
 # Log-normal transform
-pm10_transformed = functions.log_normal(pm10)
+no2_transformed = functions.log_normal(no2)
 
 # =============================================================================
 # Configuration
 # =============================================================================
 test_day_start = np.datetime64('2013-01-01', dtype='datetime64[D]')
-test_day_end = np.datetime64('2014-01-01', dtype='datetime64[D]')
+test_day_end = np.datetime64('2016-01-01', dtype='datetime64[D]')
 
 error = np.empty(((test_day_end-test_day_start).astype('int'), 5, 24))
 estmated_var = np.empty(((test_day_end-test_day_start).astype('int'), 5, 24))
 
-
 time_train = np.arange('2006-01-01', '2013-01-01', dtype='datetime64[h]')
 index_train = np.arange(np.where(date == time_train[0])[0], np.where(date == time_train[-1])[0]+1)
-data_train = pm10_transformed[:, index_train]
+data_train = no2_transformed[:, index_train]
 
 index_busday = np.is_busday(time_train.astype('datetime64[D]'))
 data_train_busday = data_train[:, index_busday].reshape(5, -1, 24) 
@@ -64,9 +63,6 @@ acf = functions.acf(data_train)
 mean_busday = np.mean(data_train_busday, axis=1)
 mean_holiday = np.mean(data_train_holiday, axis=1)
 
-NN_model_busday = load_model("bestmodel_pm10_5l_busday.h5")
-NN_model_holiday = load_model("bestmodel_pm10_5l_holiday.h5") 
-
 for day in np.arange(test_day_start, test_day_end, dtype='datetime64[D]'):
     
     t = time.time()
@@ -79,31 +75,32 @@ for day in np.arange(test_day_start, test_day_end, dtype='datetime64[D]'):
 
     index_test = np.arange(np.where(date == time_test[0])[0], np.where(date == time_test[-1])[0]+1)
 
-    data_test = pm10_transformed[:, index_test]
-    
+    data_test = no2_transformed[:, index_test]
     
     # =============================================================================
     # Prediction made by the NN
     # =============================================================================
-    meteo_test = meteo[:, index_test[0]-24:index_test[0]]
-    feature_test = np.empty((6, 24))
-    feature_test[0:4, :] = meteo_test[[0, 1, 2, 4], :]
-    feature_test[4, :] = np.sin(np.deg2rad(meteo_test[3, :]))
-    feature_test[5, :] = np.cos(np.deg2rad(meteo_test[3, :]))
-    
     if np.is_busday(time_test[0].astype('datetime64[D]')):
-        mu_test_prior = NN_model_busday.predict(np.concatenate(feature_test).reshape(1, 144))
+        NN_model = load_model("bestmodel_no2_5l_busday.h5")
     else:
-        mu_test_prior = NN_model_holiday.predict(np.concatenate(feature_test).reshape(1, 144))  
-    mu_test_prior = mu_test_prior.reshape(5, 24)
+        NN_model = load_model("bestmodel_no2_5l_holiday.h5")   
     
     # =============================================================================
     # The prior of testing data
     # =============================================================================
-#    if np.is_busday(time_test[0].astype('datetime64[D]')):
-#        mu_test_prior = mean_busday
-#    else:
-#        mu_test_prior = mean_holiday
+    if np.is_busday(time_test[0].astype('datetime64[D]')):
+        mu_test_prior = mean_busday
+    else:
+        mu_test_prior = mean_holiday
+        
+#    meteo_test = meteo[:, index_test[0]-24:index_test[0]]
+#    feature_test = np.empty((6, 24))
+#    feature_test[0:4, :] = meteo_test[[0, 1, 2, 4], :]
+#    feature_test[4, :] = np.sin(np.deg2rad(meteo_test[3, :]))
+#    feature_test[5, :] = np.cos(np.deg2rad(meteo_test[3, :]))
+    
+#    mu_test_prior = NN_model.predict(np.concatenate(feature_test).reshape(1, 144))
+#    mu_test_prior = mu_test_prior.reshape(5, 24)
         
     cov_test_prior = np.zeros((5, 24, 24))
     for i in range(5):
@@ -112,10 +109,10 @@ for day in np.arange(test_day_start, test_day_end, dtype='datetime64[D]'):
 #    fig, axs = plt.subplots(1, 5, figsize=(20, 4), sharey=True)
 #    for i in range(5):
 #        axs[i].plot(np.exp(mu_test_prior[i, :]), 'k') 
-#        axs[i].plot(pm10[i, index_test], 'k:')
+#        axs[i].plot(no2[i, index_test], 'k:')
 #        axs[i].set_xlim([0, 23])
 #        axs[i].set_xlabel(r'Time (hour)')
-#        axs[i].set_ylim([0, .8*np.max(pm10)])
+#        axs[i].set_ylim([0, .8*np.max(no2)])
 #    axs[0].set_ylabel(r'NO$_2$ ($\mu g/m^3$)')
 #    
 #    fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(24,4), sharey=True)
@@ -132,8 +129,8 @@ for day in np.arange(test_day_start, test_day_end, dtype='datetime64[D]'):
     # =============================================================================
     # Iterative updating
     # =============================================================================
-    MAX_ITERATION = 5
-    BATCH_SIZE = 24*7*32  # weeks
+    MAX_ITERATION = 1
+    BATCH_SIZE = 24*7*4  # weeks
     TOTAL_SIZE = data_train.shape[1]
     
     this_mu_test_prior = mu_test_prior
@@ -191,7 +188,7 @@ for day in np.arange(test_day_start, test_day_end, dtype='datetime64[D]'):
     mu_test_posterior_inv, cov_test_posterior_inv = functions.log_normal_inverse(this_mu_test_posterior, this_cov_test_posterior)
     
     samples_mean, samples_median, samples_percentile_high, samples_percentile_low, samples_var = functions.sample_log_normal(this_mu_test_posterior, this_cov_test_posterior, 10000, [95, 5])    
-    error[(day-test_day_start).astype('int'), :, :] = pm10[:, index_test] - samples_mean
+    error[(day-test_day_start).astype('int'), :, :] = no2[:, index_test] - samples_mean
     estmated_var[(day-test_day_start).astype('int'), :, :] = samples_var
     
     elapsed = time.time() - t
@@ -201,11 +198,11 @@ for day in np.arange(test_day_start, test_day_end, dtype='datetime64[D]'):
 #    fig, axs = plt.subplots(1, 5, figsize=(20, 4), sharey=True)
 #    for i in range(5):
 #        axs[i].plot(samples_mean[i, :], 'k') 
-#        axs[i].plot(pm10[i, index_test], 'k:')
+#        axs[i].plot(no2[i, index_test], 'k:')
 #        axs[i].fill_between(np.arange(0, 24), samples_percentile_low[i, :], samples_percentile_high[i, :], color='lightgray')
 #        axs[i].set_xlim([0, 23])
 #        axs[i].set_xlabel(r'Time (hour)')
-#        axs[i].set_ylim([0, .8*np.max(pm10)])
+#        axs[i].set_ylim([0, .8*np.max(no2)])
 #    axs[0].set_ylabel(r'NO$_2$ ($\mu g/m^3$)')
 #    
 #    fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(24,4), sharey=True)
@@ -219,20 +216,11 @@ for day in np.arange(test_day_start, test_day_end, dtype='datetime64[D]'):
 #    axes[0].set_ylabel(r'Time (hour)')
 #    plt.show()
     
-
-error_p95 = np.percentile(np.abs(error), 95, axis = 0)
-error_p5 = np.percentile(np.abs(error), 5, axis = 0)
-average_error = np.mean(np.abs(error), axis = 0)
-
-fig, axs = plt.subplots(1, 5, figsize=(20, 4), sharey=True)
+    
+mse = np.mean(np.square(error), axis = 0)
+plt.figure(figsize=(5,4))
 for i in range(5):
-    axs[i].plot(average_error[i, :], 'k')
-    axs[i].fill_between(np.arange(0, 24), error_p5[i, :], error_p95[i, :], color='lightgray')
-    axs[i].set_xlim([0, 23])
-    axs[i].set_xlabel(r'Time (hour)')
-    axs[i].set_ylim([0, 100])
-axs[0].set_ylabel(r'PM$_{10}$ ($\mu g/m^3$)')
-plt.tight_layout()
-plt.savefig("pm10_experiement_b5_bs32_withNN.pdf", format='pdf')
-
-np.save('pm10_experiement_b5_bs32_withNN', error)
+    plt.plot(mse[i, :], label = 'Station %i' % i)
+plt.xlim([0, 23])
+plt.ylim([0, 1000])
+plt.legend()
